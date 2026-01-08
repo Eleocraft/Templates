3bb1cfb39782247930a6297a35cc418b8614cb50
eleocraft <eliseo.cailloux@gmx.de>
2026-01-08 02:54:14 +0100

    adding h7skith and flake templates

---
+++ flake-templates/embassy-flake.nix
@@ -0,0 +1,50 @@
+{
+  description = "embassy g0b1 flake";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+    fenix = {
+      url = "github:nix-community/fenix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = { self, nixpkgs, fenix, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+          overlays = [ fenix.overlays.default ];
+        };
+      in
+      {
+        devShells.default =
+        let
+          toolchain = pkgs.fenix.latest.toolchain;
+          lib = pkgs.fenix.targets.thumbv6m-none-eabi.latest;
+          rust = pkgs.fenix.combine [
+            toolchain.rustc
+            toolchain.cargo
+            toolchain.rustfmt
+            toolchain.clippy
+            lib.rust-std
+          ];
+        in
+        pkgs.mkShell {
+          buildInputs = with pkgs; [
+            rust
+
+            # for flashing
+            probe-rs-tools
+
+            # for external deps
+            pkg-config
+          ];
+
+					# set default defmt log level
+					DEFMT_LOG = "info";
+        };
+      }
+    );
+}
