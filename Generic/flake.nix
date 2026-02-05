40c1f6a6a9cd52e8c42144bd51c1ed61797abd82
eleocraft <eliseo.cailloux@gmx.de>
2026-02-05 18:41:11 +0100

    adding generic flake template

---
+++ Generic/flake.nix
@@ -0,0 +1,22 @@
+{
+  description = "devshell typst";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }: 
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          nativeBuildInputs = with pkgs; [
+          {% for dep in dependencies | split(pat=",") %}  {{ dep | trim }}
+          {% endfor %}];
+        };
+      }
+    );
+}
